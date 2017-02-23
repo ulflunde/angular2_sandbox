c7e711645fda0a80cc64ce37629d79575b97681a
ulflunde <ulf.lunde@gmail.com>
2017-02-23 10:46:07 +0100

    Added contents

--- app/environment_app.component.ts
+++ app/environment_app.component.ts
@@ -1,3 +1,16 @@
 /**
  * Created by E214595 on 23.02.2017.
  */
+import {Component, View} from "angular2/core";
+
+@Component({
+    selector: 'my-app'
+})
+
+@View({
+    template: '<h2>My First Angular 2 App</h2>'
+})
+
+export class AppComponent {
+
+}
--- app/environment_main.ts
+++ app/environment_main.ts
@@ -1,3 +1,7 @@
 /**
  * Created by E214595 on 23.02.2017.
  */
+import {bootstrap} from "angular2/platform/browser"
+import {AppComponent} from "./environment_app.component"
+
+bootstrap(AppComponent);
