7e64b1e21b6ac2cd91a25d68200cbd3170c7200b
ulflunde <ulf.lunde@gmail.com>
2017-02-23 12:04:51 +0100

    https://www.tutorialspoint.com/angular2/angular2_hello_world.htm

---
+++ app/hello_world_app.component.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by E214595 on 23.02.2017.
+ */
+import {Component, View} from "angular2/core";
+
+@Component({
+    selector: 'my-app'
+})
+
+@View({
+    template: '<h2>Hello World !!</h2>'
+})
+
+export class MyHelloWorldClass {
+
+}
---
+++ app/hello_world_main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by E214595 on 23.02.2017.
+ */
+import {bootstrap} from "angular2/platform/browser"
+import {MyHelloWorldClass} from "./hello_world_app.component"
+
+bootstrap(MyHelloWorldClass);
